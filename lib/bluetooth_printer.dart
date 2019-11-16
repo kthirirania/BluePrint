e88bb059f190ebdc316a850083609587726a1439
medyas <medyassinesabri@gmail.com>
2019-11-16 12:24:59 +0100

    stream-hotfix

--- lib/bluetooth_printer.dart
+++ lib/bluetooth_printer.dart
@@ -8,26 +8,29 @@ import 'package:flutter/services.dart';
 
 class BluetoothPrinter {
   static const MethodChannel _channel =
-      const MethodChannel('bluetooth_printer/methodChannel');
+  const MethodChannel('bluetooth_printer/methodChannel');
 
   /*事件通道*/
   static const EventChannel _scanBlueToothEvent =
-      const EventChannel('bluetooth_printer/scanBlueToothEvent');
+  const EventChannel('bluetooth_printer/scanBlueToothEvent');
+
+  Stream<List<Map>> _stream;
 
   /*开始扫描蓝牙*/
   Future startScanBlueTooth() async {
     await _channel.invokeMethod('startScanBlueTooth');
   }
+
   Future<List<dynamic>> getBoundDevices() async {
-    if(Platform.isIOS) return null;
+    if (Platform.isIOS) return null;
     return await _channel.invokeMethod('getBoundDevices');
   }
 
   /*连接蓝牙设备*/
   Future<bool> connectBlueTooth(int index) async {
     int result =
-        await _channel.invokeMethod('connectBlueTooth', {'index': index});
-    return result==1;
+    await _channel.invokeMethod('connectBlueTooth', {'index': index});
+    return result == 1;
   }
 
   /*打印*/
@@ -55,12 +58,19 @@ class BluetoothPrinter {
   }
 
   Future<void> destroy() async {
-    if(Platform.isIOS) return;
+    if (Platform.isIOS) return;
     await _channel.invokeMethod('destroy');
   }
 
   /*监听扫描蓝牙设备回调事件*/
-  Stream<List<Map>> get scanBlueToothEvent => _scanBlueToothEvent
-      .receiveBroadcastStream().asBroadcastStream();
-       /*.map<Map>((data) => data).toList()*/
+  Stream<List<Map>> get scanBlueToothEvent {
+    if (_stream == null) {
+      _stream = _scanBlueToothEvent
+          .receiveBroadcastStream().map((dynamic map) =>
+      Map<String,
+          dynamic>.from(map));
+      /*.map<Map>((data) => data).toList()*/
+    }
+    return _stream;
+  }
 }
